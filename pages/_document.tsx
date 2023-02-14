ad1930856d4e84279e08eef5049a5e624d73ba37
Samantha2233 <sami.shackelford@gmail.com>
2023-02-14 00:54:40 -0500

    start project and add fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,23 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="preload"
+          href="fonts/Inter-Black.ttf"
+          as="font"
+          type="font/ttf"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="fonts/Inter-Light.ttf"
+          as="font"
+          type="font/ttf"
+          crossOrigin="anonymous"
+        />
+        
+      </Head>
       <body>
         <Main />
         <NextScript />
