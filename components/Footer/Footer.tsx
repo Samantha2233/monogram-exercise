654e018acb6fb42132e12efb9cbb7cb7b49d5a88
Samantha2233 <sami.shackelford@gmail.com>
2023-02-16 18:10:47 -0500

    add copyright

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,7 +1,8 @@
 export function Footer() {
     return (
         <div className='bg-[#151515] flex flex-col items-center pt-[245px] pb-[156px]'>
-            <p className='text-white text-center text-[24px] leading-[162%] px-[10px] pb-[30px]'>This is a design test by Sami Shackelford for Monogram.</p>
+            <p className='text-white text-center text-[24px] leading-[162%] px-[10px] pb-[10px]'>This is a design test by Sami Shackelford for Monogram.</p>
+            <p className='text-gray-500'>© Monogram. All rights reserved.</p>
             <span
                 className="text-[#FF069C] text-[54px]"
                 role="img"
