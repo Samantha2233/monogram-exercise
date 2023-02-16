b262d59957128d67585e99b41e113332252ab473
Samantha2233 <sami.shackelford@gmail.com>
2023-02-16 17:46:34 -0500

    footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,7 +1,14 @@
 export function Footer() {
     return (
-        <div>
-            
+        <div className='bg-[#151515] flex flex-col items-center pt-[245px] pb-[156px]'>
+            <p className='text-white text-center text-[24px] leading-[162%] px-[10px] pb-[30px]'>This is a design test by Sami Shackelford for Monogram.</p>
+            <span
+                className="text-[#FF069C] text-[54px]"
+                role="img"
+                aria-label="heart"
+            >
+                ❤️
+            </span>
         </div>
     )
 }
